35002b17ce79a2b9ac7ce638dc97ba1875bc0417
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2023-01-27 11:46:23 -0500

    Update leap.cpp

--- leap.cpp
+++ leap.cpp
@@ -7,3 +7,32 @@ Wrote a program that asks the user to input an integer representing a year numbe
 If the input year is a leap year according to the modern Gregorian calendar, it should print Leap year, otherwise, print Common year.
 */
 
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  int year;
+  cout << "Enter year: " << endl;
+  cin >> year;
+  
+    if (!(year%4 == 0))
+    {
+     cout << "Common year " << endl;
+    }
+    
+  else if (!(year%100 == 0))
+  {
+    cout << "Leap year " << endl;
+  }
+   
+  else if (!(year%400 == 0))
+  {
+    cout << "Common year " << endl;
+  }
+   
+  else 
+  {
+    cout << "Leap year " << endl;
+  }
+}
